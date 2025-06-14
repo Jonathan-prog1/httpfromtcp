863782bd0becc8993031a03dbc68498fd156c0b3
Jonathan-prog1 <jmcapp99@outlook.com>
2025-06-14 16:47:50 -0700

    Refactor code to read a file in a func

--- main.go
+++ main.go
@@ -19,43 +19,53 @@ func main() {
 		// If an error occurs, log the error and terminate the program
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	// Ensure the file is closed when the function returns
-	defer f.Close()
 
 	// Print a message indicating that data is being read from the file
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
-	// Holds the current line
-	currentLineContents := ""
+	lineschan := getLinesChannel(f)
 
-	// Infinite loop to read the file in chunks
-	for {
-		// Create a buffer to hold the data read from the file
-		buffer := make([]byte, 8, 8)
-		// Read data into the buffer
-		n, err := f.Read(buffer)
-		if err != nil {
-			// This sees if the current line is empty
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
+	for line := range lineschan {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		// Holds the current line
+		currentLineContents := ""
+		// Infinite loop to read the file in chunks
+		for {
+			// Create a buffer to hold the data read from the file
+			buffer := make([]byte, 8, 8)
+			// Read data into the buffer
+			n, err := f.Read(buffer)
+			if err != nil {
+				// This sees if the current line is empty
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				// Check if the end of the file has been reached
+				if errors.Is(err, io.EOF) {
+					break // Exit the loop if the end of the file is reached
+				}
+				// If another error occurs, print the error and exit the loop
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+			// Convert the read bytes into a string and print it
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
 				currentLineContents = ""
 			}
-			// Check if the end of the file has been reached
-			if errors.Is(err, io.EOF) {
-				break // Exit the loop if the end of the file is reached
-			}
-			// If another error occurs, print the error and exit the loop
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			currentLineContents += parts[len(parts)-1]
 		}
-		// Convert the read bytes into a string and print it
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
-	}
+	}()
+	return lines
 }

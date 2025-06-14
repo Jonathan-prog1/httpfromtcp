3845e66b6abc47655115867b6bb9838b76439085
Jonathan-prog1 <jmcapp99@outlook.com>
2025-06-14 16:23:46 -0700

    read file line by line

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"     // Provides basic I/O primitives
 	"log"    // Provides logging functions
 	"os"     // Provides functions for OS-level operations
+	"strings"
 )
 
 // Define the path to the input file
@@ -25,13 +26,21 @@ func main() {
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
+	// Holds the current line
+	currentLineContents := ""
+
 	// Infinite loop to read the file in chunks
 	for {
 		// Create a buffer to hold the data read from the file
-		b := make([]byte, 8, 8)
+		buffer := make([]byte, 8, 8)
 		// Read data into the buffer
-		n, err := f.Read(b)
+		n, err := f.Read(buffer)
 		if err != nil {
+			// This sees if the current line is empty
+			if currentLineContents != "" {
+				fmt.Printf("read: %s\n", currentLineContents)
+				currentLineContents = ""
+			}
 			// Check if the end of the file has been reached
 			if errors.Is(err, io.EOF) {
 				break // Exit the loop if the end of the file is reached
@@ -41,7 +50,12 @@ func main() {
 			break
 		}
 		// Convert the read bytes into a string and print it
-		str := string(b[:n])
-		fmt.Printf("read: %s\n", str)
+		str := string(buffer[:n])
+		parts := strings.Split(str, "\n")
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
+			currentLineContents = ""
+		}
+		currentLineContents += parts[len(parts)-1]
 	}
 }
